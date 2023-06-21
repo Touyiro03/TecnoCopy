b5d4bf88bd9b38fe5fb2451cd4a2f5a75a67aca2
Touyiro03 <103567694+Touyiro03@users.noreply.github.com>
2023-06-21 14:27:15 -0600

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './lib/gradients.ts'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-accent-1': '#111111',
+        'dark-accent-2': '#333333',
+        'dark-accent-3': '#444444',
+        'dark-accent-5': '#888888'
+      }
+    }
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
 };
