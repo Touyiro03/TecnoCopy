e4ffdeba766145606d83825da243fe0e44502ec0
Touyiro03 <103567694+Touyiro03@users.noreply.github.com>
2023-06-30 12:14:19 -0600

    padding

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,7 +10,7 @@ export default function Home() {
     <Grid
       container
       spacing={3}
-      sx={{ p: 2 }}
+      sx={{ p: 3 }}
     >
       <Grid item xs={12} lg={6}>
         <Card sx={{ boxShadow: 3, borderRadius: 3 }}>
